7cfe4e2ce4e81e605181378351d535c2c4a7f173
Alex Akagi <alex@akagi.co>
2025-07-10 14:13:41 -0400

    Use `__*.zsh` for private configs

--- zdotdir/custom/dx.zsh
+++ zdotdir/custom/dx.zsh
@@ -8,3 +8,9 @@ alias c="code ."
 
 # Automatically make `mv` safe and verbose
 alias mv='mv -vn'
+
+# Run `tree` command for active & maintained projects
+alias lsproj="tree -d -L 2 -I 'archive|$sandbox_dirname' $proj"
+
+# Disable Power Chime (I don't think this works anymore, need to double check)
+alias kp="defaults write com.apple.PowerChime ChimeOnNoHardware -bool true && killall PowerChime"
---
+++ zdotdir/custom/functions/ldarkly.zsh
@@ -0,0 +1,25 @@
+ldarkly() {
+  # Initialize flag_id as empty
+  flag_id=""
+
+  # Default selected-env to test
+  selected_env="test"
+
+  # Loop through all arguments
+  for arg in "$@"; do
+    if [[ "$arg" == "--prod" || "$arg" == "-p" ]]; then
+      # Set the selected environment to production if --prod or -p is passed
+      selected_env="production"
+    elif [[ "$arg" != --* && -z "$flag_id" ]]; then
+      # If the argument doesn't start with '--' and flag_id is not set, use it as flag_id
+      flag_id=$(echo "$arg" | tr '[:upper:]' '[:lower:]')
+    fi
+  done
+
+  if [ -z "$flag_id" ]; then
+    # No argument, open the default URL
+    open "https://app.launchdarkly.com/projects/default/flags?env=production&env=test&selected-env=${selected_env}"
+  else
+    open "https://app.launchdarkly.com/projects/default/flags/${flag_id}/targeting?env=production&env=test&selected-env=${selected_env}"
+  fi
+}
